bab2350e053e648df07e32ec1d16a0840d01a6d3
Lucas Nery <neryluc@gmail.com>
2015-10-08 14:23:47 -0300

    Adicionando uma nova linha idiota

--- Teste.java
+++ Teste.java
@@ -1,5 +1,6 @@
 public class Teste{
 	public static void main(String[] args){
 		System.out.println("Imprimindo Teste na tela.\n");
+		System.out.println("Adicionando uma nova linha inutil.\n");
 	}
 }
