5414260b6da67c37cf265e83d2b394cd0f1b3149
Lucas Nery <lucas.nery@pitang.com>
2015-10-29 14:13:40 -0300

    Adicionando sobrenome no Usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,9 +1,11 @@
 public class Usuario{
 	private int idade;
 	private String nome;
+	private String sobrenome;
 
-	public Usuario(int idade, String nome){
+	public Usuario(int idade, String nome, String sobrenome){
 		this.idade = idade;
 		this.nome = nome;
+		this.sobrenome = sobrenome;
 	}
 }
